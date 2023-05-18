a15404cd4244609c8b3b56c17f8e6dc8750480ca
Shiv Sankhavaram <ssankha@purdue.edu>
2023-05-17 19:13:21 -0700

    created nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SideNavigation from "@cloudscape-design/components/side-navigation";
+import { AppLayout } from '@cloudscape-design/components';
+
+function currentContent(activeRef) {
+
+  if(activeRef === "#/employees"){
+    return "employees";
+  }
+  
+  if(activeRef === "#/reports") {
+    return "reports";
+  }
+
+  if(activeRef.includes("employees")) {
+    return "employee";
+  }
+
+  return "report";
+}
+
 
 function App() {
+  const [activeHref, setActiveHref] = useState(
+    "#/employees");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppLayout
+      navigation={
+        <SideNavigation
+          activeHref={activeHref}
+          header={{ href: "#/", text: "Navigation" }}
+          onFollow={event => {
+            if (!event.detail.external) {
+              event.preventDefault();
+              setActiveHref(event.detail.href);
+            }
+          }}
+          items={[
+            {
+              type: "expandable-link-group",
+              text: "Employees",
+              href: "#/employees",
+              items: [
+                {
+                  type: "link",
+                  text: "Employee 1",
+                  href: "#/employees/employee1"
+                },
+                {
+                  type: "link",
+                  text: "Employee 2",
+                  href: "#/employees/employee2"
+                },
+                {
+                  type: "link",
+                  text: "Employee 3",
+                  href: "#/employees/employee3"
+                }
+              ]
+            },
+            {
+              type: "expandable-link-group",
+              text: "Reports",
+              href: "#/reports",
+              items: [
+                {
+                  type: "link",
+                  text: "Performance",
+                  href: "#/reports/performance"
+                },
+                {
+                  type: "link",
+                  text: "Behavior",
+                  href: "#/reports/behavior"
+                }
+              ]
+            },
+          ]}
+        />
+      }
+      
+      content={currentContent(activeHref)}>
+    </AppLayout>
   );
 }
 
